74990fb5b4c4ec3f11fbae99de04b6ca21eafd44
Josh Canton <joshcanton6@gmail.com>
2024-12-20 11:17:17 -0800

    Update main.js
    Debugging

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ async function redirect() {
 }
 
 async function debugAction() {
-    document.getElementById("debug-data").innerHTML = JSON.stringify(await Spotify.getUsersTopItems(sessionStorage.getItem("access_token"), "artists", "short_term"));
+    let data = await Spotify.getUsersTopItems(sessionStorage.getItem("access_token"), "artists", "short_term");
 }
 
 // #endregion Functions
