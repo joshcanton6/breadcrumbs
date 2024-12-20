a81fa0486f521ad1f9b94c280e0d4eae53a5049c
Josh Canton <joshcanton6@gmail.com>
2024-12-20 09:35:52 -0800

    Update main.js
    Updated variables

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ import * as Spotify from "./spotify.js"
 // #region Constants
 
 const home = "https://joshcanton6.github.io/breadcrumbs"
-const homePathname = "/breadcrumbs"
-const redirect_uri = home + "/redirect";
-const client_id = "70d3f1361abf4e1ab9e9e64089fabc36";
+const redirectURI = home + "/redirect";
+const clientID = "70d3f1361abf4e1ab9e9e64089fabc36";
 const spotifyLoginButton = document.getElementById("spotify-login-button");
 
 // #endregion Constants
@@ -23,9 +22,9 @@ function login() {
         " user-top-read";
 
     window.location.href = "https://accounts.spotify.com/authorize" +
-        "?client_id=" + client_id +
+        "?client_id=" + clientID +
         "&response_type=code" +
-        "&redirect_uri=" + encodeURIComponent(redirect_uri) +
+        "&redirect_uri=" + encodeURIComponent(redirectURI) +
         "&scope=" + encodeURIComponent(scope) +
         "&show_dialog=true";
 }
@@ -44,7 +43,7 @@ async function redirect() {
                 "&redirect_uri=" + encodeURIComponent(redirect_uri),
             headers: {
                 "content-type": "application/x-www-form-urlencoded",
-                "Authorization": "Basic " + btoa(client_id + ":2f409da2aebf417893ff056f9b98c3ea")
+                "Authorization": "Basic " + btoa(clientID + ":2f409da2aebf417893ff056f9b98c3ea")
             }
         }).then(
             (response) => response.json()
@@ -54,17 +53,11 @@ async function redirect() {
         sessionStorage.setItem("refresh_token", token["refresh_token"]);
         sessionStorage.setItem("expires_at", Math.floor(Date.now() / 1000) + token["expires_in"]);
 
-        window.location.pathname = homePathname + "/app";
+        window.location.href = home + "/app";
     }
 }
 
 // #endregion Functions
-
-// #region Handlers
-
-// TODO
-
-// #endregion Handlers
 
 // #region Listeners
 
