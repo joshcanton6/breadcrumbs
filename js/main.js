7c75db9b2c3a653f71b7a9c1782f69321adbe592
Josh Canton <joshcanton6@gmail.com>
2024-12-27 15:04:36 -0800

    Update main.js
    Improved token management:
    - consistency in global variables
    - implemented URL search params
    - added token refresh

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ import * as Spotify from "./spotify.js"
 // #region Constants
 
 const home = "https://joshcanton6.github.io/breadcrumbs"
-const redirectURI = home + "/redirect";
-const clientID = "70d3f1361abf4e1ab9e9e64089fabc36";
+const redirect_uri = home + "/redirect";
+const client_id = "70d3f1361abf4e1ab9e9e64089fabc36";
+const scope = "user-top-read";
 const spotifyLoginButton = document.getElementById("spotify-login-button");
 const debugButton = document.getElementById("debug-button");
 
@@ -12,18 +13,13 @@ const debugButton = document.getElementById("debug-button");
 // #region Functions
 
 function login() {
-    const scopes = [
-        "user-top-read"
-    ];
-    const scope = scopes.join(" ");
-    const authOptions = `?${[
-        `client_id=${clientID}`,
-        "response_type=code",
-        `redirect_uri=${encodeURIComponent(redirectURI)}`,
-        `scope=${encodeURIComponent(scope)}`,
-        "show_dialog=true"
-    ].join("&")}`;
-    window.location.href = "https://accounts.spotify.com/authorize" + authOptions;
+    const authOptions = new URLSearchParams({
+        client_id,
+        "response_type": "code",
+        redirect_uri,
+        scope
+    });
+    window.location.href = `https://accounts.spotify.com/authorize?${authOptions}`;
 }
 
 async function redirect() {
@@ -32,19 +28,18 @@ async function redirect() {
         document.getElementById("redirect-message").innerHTML = `Error: ${urlParams.get("error")}`;
     }
     if (urlParams.has("code")) {
-        const response = await fetch("https://accounts.spotify.com/api/token", {
+        const token = (await fetch("https://accounts.spotify.com/api/token", {
             method: "POST",
-            body: [
-                "grant_type=authorization_code",
-                `code=${urlParams.get("code")}`,
-                `redirect_uri=${encodeURIComponent(redirectURI)}`
-            ].join("&"),
             headers: {
                 "content-type": "application/x-www-form-urlencoded",
-                "Authorization": `Basic ${btoa(`${clientID}:2f409da2aebf417893ff056f9b98c3ea`)}`
-            }
-        });
-        const token = await response.json();
+                "Authorization": `Basic ${btoa(`${client_id}:2f409da2aebf417893ff056f9b98c3ea`)}`
+            },
+            body: new URLSearchParams({
+                "grant_type": "authorization_code",
+                "code": urlParams.get("code"),
+                redirect_uri
+            })
+        })).json();
         sessionStorage.setItem("access_token", token["access_token"]);
         sessionStorage.setItem("refresh_token", token["refresh_token"]);
         sessionStorage.setItem("expires_at", Math.floor(Date.now() / 1000) + token["expires_in"]);
@@ -52,12 +47,28 @@ async function redirect() {
     }
 }
 
-function getToken() {
+async function getToken() {
+    if (sessionStorage.getItem("expires_at") - Math.floor(Date.now() / 1000) <= 0) {
+        await refreshToken();
+    }
     return sessionStorage.getItem("access_token");
 }
 
-function refreshToken() {
-    
+async function refreshToken() {
+    const token = (await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            "grant_type": "refresh_token",
+            "refresh_token": sessionStorage.getItem("refresh_token"),
+            client_id
+        })
+    })).json();
+    sessionStorage.setItem("access_token", token["access_token"]);
+    sessionStorage.setItem("refresh_token", token["refresh_token"]);
+    sessionStorage.setItem("expires_at", Math.floor(Date.now() / 1000) + token["expires_in"]);
 }
 
 async function debugAction() {
@@ -69,7 +80,7 @@ async function debugAction() {
 // #region Listeners
 
 if (spotifyLoginButton) spotifyLoginButton.addEventListener("click", login);
-if (window.location.origin + window.location.pathname == redirectURI) redirect();
+if (window.location.origin + window.location.pathname == redirect_uri) redirect();
 if (debugButton) debugButton.addEventListener("click", debugAction);
 
 // #endregion Listeners
