5954826625cbae44444e1a8363b39ad8224cb49b
Josh Canton <joshcanton6@gmail.com>
2024-12-31 16:15:45 -0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+import * as Spotify from "./spotify.js"
+import * as OAuth from "./auth.js"
+
+async function insertTopArtists() {
+    const topArtists = await Spotify.getUsersTopItems(await OAuth.getToken(), "artists", "short_term", 10);
+    let innerHTMLContent = "";
+    for (const artist of topArtists["items"]) {
+        innerHTMLContent += `
+            <tr>
+                <td><input type="checkbox" name="artist" value="${artist["id"]}"></td><td>${artist["name"]}</td>
+            </tr>
+        `
+    }
+    document.getElementById("top-artists").innerHTML = innerHTMLContent;
+}
+
+async function poke() {
+    /* edit this function for testing */
+}
+
+window.poke = poke;
